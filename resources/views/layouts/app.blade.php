06ac41172fe30402d1344a3009e6f682db5dc443
Ged <ggedanthony@gmail.com>
2020-01-20 19:08:17 +0800

    Set APP_KEY

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
     <title>ConsTrak</title>
 
+    {{-- Just a comment --}}
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
